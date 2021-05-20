979a414fab96dd693406ea844f8d3b9e03076085
Elton F Oliveira <eltonf.oliveira22@gmail.com>
2021-05-20 15:27:44 -0300

    PrestadorServicoDTO/TestExameMedicoDTO

--- src/main/java/br/com/proway/senior/godevcadastrogrupo1/model/DTO/ExameMedicoDTO.java
+++ src/main/java/br/com/proway/senior/godevcadastrogrupo1/model/DTO/ExameMedicoDTO.java
@@ -2,36 +2,47 @@ package br.com.proway.senior.godevcadastrogrupo1.model.DTO;
 
 import java.time.LocalDate;
 
+import br.com.proway.senior.godevcadastrogrupo1.model.ExameMedico;
 import br.com.proway.senior.godevcadastrogrupo1.utils.EnumExamesMedicos.TiposExames;
 
+/**
+* Classe ExameMedicoDTO.
+* 
+* Classe para interação via Controller API, tem referência com {@link Colaborador}.
+* 
+* @author Elton Oliveira <elton.oliveira@senior.com.br>
+ */
 public class ExameMedicoDTO {
 	
 	private int id;
 	TiposExames tipoExame;
 	private LocalDate dataExame;
+	private boolean apto;
 	
-	public ExameMedicoDTO(ExameMedicoDTO exameMedicoDTO) {
-		this.id = exameMedicoDTO.getId();
-		this.tipoExame = exameMedicoDTO.getTipoExame();
-		this.dataExame = exameMedicoDTO.getDataExame();
+	public ExameMedicoDTO(ExameMedico exameMedico) {
+		this.id = exameMedico.getId();
+		this.tipoExame = exameMedico.getTipoExame();
+		this.dataExame = exameMedico.getDataExame();
+		this.apto = exameMedico.isApto();
 	}
+
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public TiposExames getTipoExame() {
 		return tipoExame;
 	}
-	public void setTipoExame(TiposExames tipoExame) {
-		this.tipoExame = tipoExame;
-	}
+
 	public LocalDate getDataExame() {
 		return dataExame;
 	}
-	public void setDataExame(LocalDate dataExame) {
-		this.dataExame = dataExame;
+
+	public boolean isApto() {
+		return apto;
 	}
+
+
+	
 
 }
