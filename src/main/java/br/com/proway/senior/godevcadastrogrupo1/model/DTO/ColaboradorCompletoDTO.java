5743373fd10b2e03169aa8397ad6aebc07592869
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-20 14:45:49 -0300

    Classe criada e documentada

---
+++ src/main/java/br/com/proway/senior/godevcadastrogrupo1/model/DTO/ColaboradorCompletoDTO.java
@@ -0,0 +1,19 @@
+package br.com.proway.senior.godevcadastrogrupo1.model.DTO;
+
+import br.com.proway.senior.godevcadastrogrupo1.model.Colaborador;
+
+/**
+ * Classe ColaboradorCompletoDTO.
+ * 
+ * Classe para interação via Controller API, referencia a {@link Colaborador}.
+ * Oferece as informações completas do colaborador, podendo ser utilizada para 
+ * relatórios. As informações simplificadas para os demais módulos do sistema,
+ * estão disponíveis em {@link ColaboradorSimplificadoDTO}.
+ * 
+ * @author Sarah Neuburger Brito <b>sarah.brito@senior.com.br</b>
+ */
+public class ColaboradorCompletoDTO {
+
+	
+	
+}
