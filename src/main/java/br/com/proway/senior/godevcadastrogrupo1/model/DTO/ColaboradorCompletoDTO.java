570140e4622518d60315458a7ea8e837df904a75
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-20 14:50:32 -0300

    Atributos criados

--- src/main/java/br/com/proway/senior/godevcadastrogrupo1/model/DTO/ColaboradorCompletoDTO.java
+++ src/main/java/br/com/proway/senior/godevcadastrogrupo1/model/DTO/ColaboradorCompletoDTO.java
@@ -1,6 +1,14 @@
 package br.com.proway.senior.godevcadastrogrupo1.model.DTO;
 
+import java.time.LocalDate;
+
 import br.com.proway.senior.godevcadastrogrupo1.model.Colaborador;
+import br.com.proway.senior.godevcadastrogrupo1.model.Conta;
+import br.com.proway.senior.godevcadastrogrupo1.model.Contatos;
+import br.com.proway.senior.godevcadastrogrupo1.model.Dependente;
+import br.com.proway.senior.godevcadastrogrupo1.model.Endereco;
+import br.com.proway.senior.godevcadastrogrupo1.model.ExameMedico;
+import br.com.proway.senior.godevcadastrogrupo1.utils.EnumDadosPessoais.IdentidadeGenero;
 
 /**
  * Classe ColaboradorCompletoDTO.
@@ -14,6 +22,31 @@ import br.com.proway.senior.godevcadastrogrupo1.model.Colaborador;
  */
 public class ColaboradorCompletoDTO {
 
-	
+	private Integer id;
+	private String nome;
+	private String sobrenome;
+	private String nomeSocial;
+	private LocalDate dataDeNascimento;
+	private String nacionalidade;
+	private String naturalidade;
+	private Boolean pcd; 
+	private String genero; 
+	private IdentidadeGenero identidadeGenero;
+	private Endereco endereco; 
+	private String cpf; 
+	private String rg; 
+	private Contatos contatos; 
+	private Integer idPostoDeTrabalho;
+	private Integer nit; 
+	private Boolean optanteVT; 
+	private Boolean optanteVAVR;
+	private LocalDate dataAdmissao;
+	private Boolean optanteDependente;
+	private String registro_alistamento; 
+	private String email_corporativo;
+	private String titulo_eleitor;
+	private Conta conta; 
+	private ExameMedico exameMedico;
+	private Dependente dependente;
 	
 }
