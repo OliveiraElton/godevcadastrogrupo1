8b195ee59d6b847db6e7e15569985efcd3c0d575
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-21 13:26:01 -0300

    Refatoração EmpresaControllerTest, buscarId, buscarNome, buscarTodas

--- src/test/java/controller/EmpresaControllerTest.java
+++ src/test/java/controller/EmpresaControllerTest.java
@@ -17,6 +17,7 @@ import br.com.proway.senior.godevcadastrogrupo1.model.Empresa;
 import br.com.proway.senior.godevcadastrogrupo1.model.Endereco;
 import br.com.proway.senior.godevcadastrogrupo1.model.DAO.EmpresaDAO;
 import br.com.proway.senior.godevcadastrogrupo1.persistence.DBConnection;
+
 /**
  * Classe EmpresaControllerTest.
  * 
@@ -31,14 +32,15 @@ public class EmpresaControllerTest {
 	static Session session = DBConnection.getSession();
 	static EmpresaDAO dao = EmpresaDAO.getInstance(session);
 	static Contatos contatos = new Contatos("478889996655", "47985556633", "empresa@gmail.com", "47987456321");
-	static Endereco endereco = new Endereco("Rua Principal", 15, "Nada consta", "Centro", "89789456", "Brasil", "Blumenau", "SC");
+	static Endereco endereco = new Endereco("Rua Principal", 15, "Nada consta", "Centro", "89789456", "Brasil",
+			"Blumenau", "SC");
 	static Empresa empresa = new Empresa("Senior", LocalDate.now(), "12345678", null, null);
 
 	@Test
 	public void testCriarEmpresa() {
-		Empresa empresaCriada = EmpresaController.criarEmpresa("Cooper", LocalDate.of(2019, 12, 31), "78456963000115", "4798888556633", 
-				"47888997852", "atendimento@cooper.com", 
-				"4788896655", "Rua XV", 78, "Próximo à", "89036789", "Escola Agrícola", "Brasil", "Blumenau", "SC");
+		Empresa empresaCriada = EmpresaController.criarEmpresa("Cooper", LocalDate.of(2019, 12, 31), "78456963000115",
+				"4798888556633", "47888997852", "atendimento@cooper.com", "4788896655", "Rua XV", 78, "Próximo à",
+				"89036789", "Escola Agrícola", "Brasil", "Blumenau", "SC");
 		assertEquals("Cooper", empresaCriada.getNomeEmpresa());
 		assertEquals("78456963000115", empresaCriada.getCnpj());
 		assertEquals("Escola Agrícola", empresaCriada.getEndereco().getBairro());
@@ -46,20 +48,20 @@ public class EmpresaControllerTest {
 
 	@Test
 	public void testDeleteEmpresa() {
-		Empresa empresaCriada = EmpresaController.criarEmpresa("Viacred", LocalDate.of(218, 12, 31), "78456963000115", "4798888556633", 
-				"47888997852", "atendimento@cooper.com", 
-				"4788896655", "Rua XV", 78, "Próximo à", "89036789", "Centro", "Brasil", "Blumenau", "SC");
+		Empresa empresaCriada = EmpresaController.criarEmpresa("Viacred", LocalDate.of(218, 12, 31), "78456963000115",
+				"4798888556633", "47888997852", "atendimento@cooper.com", "4788896655", "Rua XV", 78, "Próximo à",
+				"89036789", "Centro", "Brasil", "Blumenau", "SC");
 		EmpresaController.deleteEmpresa(empresaCriada);
 		assertNull(EmpresaController.buscarEmpresaPorId(empresaCriada.getId()));
 	}
 
 	@Test
 	public void testAtualizarEmpresa() {
-		Empresa empresaCriada = EmpresaController.criarEmpresa("Caixa", LocalDate.of(2019, 12, 31), "78456963000115", "4798888556633", 
-				"47888997852", "atendimento@caixa.com", 
-				"4788896655", "Rua XV", 78, "Próximo à", "89036789", "Escola Agrícola", "Brasil", "Blumenau", "SC");
-		Empresa empresaAlterada = EmpresaController.atualizarEmpresa(empresaCriada.getId(), "Caixa Econômica", LocalDate.of(2021, 03, 31), 
-				"89456789000123", contatos, endereco);
+		Empresa empresaCriada = EmpresaController.criarEmpresa("Caixa", LocalDate.of(2019, 12, 31), "78456963000115",
+				"4798888556633", "47888997852", "atendimento@caixa.com", "4788896655", "Rua XV", 78, "Próximo à",
+				"89036789", "Escola Agrícola", "Brasil", "Blumenau", "SC");
+		Empresa empresaAlterada = EmpresaController.atualizarEmpresa(empresaCriada.getId(), "Caixa Econômica",
+				LocalDate.of(2021, 03, 31), "89456789000123", contatos, endereco);
 		assertEquals(empresaCriada.getId(), empresaAlterada.getId());
 		assertNotEquals("Caixa", empresaAlterada.getNomeEmpresa());
 		assertEquals("Caixa Econômica", empresaAlterada.getNomeEmpresa());
@@ -68,23 +70,47 @@ public class EmpresaControllerTest {
 
 	@Test
 	public void testBuscarEmpresaPorId() {
-		Empresa empresaCriada = EmpresaController.criarEmpresa("Selecionar", LocalDate.of(2019, 12, 31), "78456963000115", "4798888556633", 
-				"47888997852", "atendimento@selecionar.com", 
-				"4788896655", "Rua XV", 78, "Próximo à", "89036789", "Victor Konder", "Brasil", "Blumenau", "SC");
-		session.clear();
-		dao.create(empresa);
-		assertEquals(empresa, EmpresaController.buscarEmpresaPorId(empresa.getId()));
+		Empresa empresaCriada = EmpresaController.criarEmpresa("Selecionar", LocalDate.of(2019, 12, 31),
+				"78456963000115", "4798888556633", "47888997852", "atendimento@selecionar.com", "4788896655", "Rua XV",
+				78, "Próximo à", "89036789", "Victor Konder", "Brasil", "Blumenau", "SC");
+		Empresa empresaRetornada = EmpresaController.buscarEmpresaPorId(empresaCriada.getId());
+		assertEquals(empresaCriada.getId(), empresaRetornada.getId());
+		assertEquals(empresaCriada.getCnpj(), empresaRetornada.getCnpj());
+		assertEquals(empresaCriada.getContato(), empresaRetornada.getContato());
+		assertEquals(empresaCriada.getDataInicioContrato(), empresaRetornada.getDataInicioContrato());
+		assertEquals(empresaCriada.getEndereco(), empresaRetornada.getEndereco());
+		assertEquals(empresaCriada.getNomeEmpresa(), empresaRetornada.getNomeEmpresa());
 	}
 
 	@Test
 	public void testBuscarTodasEmpresas() {
-		Empresa empresaCriada = EmpresaController.criarEmpresa("Hering", LocalDate.of(2019, 12, 31), "45123987000123", "4798888556633", 
-				"47888997852", "atendimento@hering.com.br", 
-				"4788896655", "Rua XV", 78, "Próximo à", "89036789", "Bom Retiro", "Brasil", "Blumenau", "SC");
-		session.clear();
-		dao.create(empresa);
-		List<Empresa> empresa2 = EmpresaController.buscarTodasEmpresas();
-		assertEquals("Senior", empresa2.get(empresa2.size() - 1).getNomeEmpresa());
+		Empresa empresaCriada1 = EmpresaController.criarEmpresa("Hering", LocalDate.of(2019, 12, 31), "45123987000123",
+				"4798888556633", "47888997852", "atendimento@hering.com.br", "4788896655", "Rua XV", 78, "Próximo à",
+				"89036789", "Bom Retiro", "Brasil", "Blumenau", "SC");
+		Empresa empresaCriada2 = EmpresaController.criarEmpresa("Marisa", LocalDate.of(2018, 12, 31), "78963258000178",
+				"4798888556633", "47888997852", "atendimento@marisa.com.br", "4788896655", "Rua XV", 78, "Próximo à",
+				"89036789", "Bom Retiro", "Brasil", "Blumenau", "SC");
+		List<Empresa> listaEmpresas = EmpresaController.buscarTodasEmpresas();
+		assertEquals(2, listaEmpresas.size());
+		assertEquals(empresaCriada1.getNomeEmpresa(), listaEmpresas.get(0).getNomeEmpresa());
+		assertEquals(empresaCriada1.getContato(), listaEmpresas.get(0).getContato());
+		assertEquals(empresaCriada1.getDataInicioContrato(), listaEmpresas.get(0).getDataInicioContrato());
+		assertEquals(empresaCriada2.getNomeEmpresa(), listaEmpresas.get(1).getNomeEmpresa());
+		assertEquals(empresaCriada2.getCnpj(), listaEmpresas.get(1).getCnpj());
+		assertEquals(empresaCriada2.getContato().getEmail(), listaEmpresas.get(1).getContato().getEmail());
+
+	}
+
+	@Test
+	public void testBuscarEmpresaPorNome() {
+		EmpresaController.criarEmpresa("Magalu Rua XV", LocalDate.of(2019, 12, 31), "45123987000123", "4798888556633",
+				"47888997852", "atendimento@magalu.com.br", "4788896655", "Rua XV", 78, "Próximo à", "89036789",
+				"Bom Retiro", "Brasil", "Blumenau", "SC");
+		EmpresaController.criarEmpresa("Magalu Centro", LocalDate.of(2018, 12, 31), "78963258000178", "4798888556633",
+				"47888997852", "atendimento@magalu.com.br", "4788896655", "Rua XV", 78, "Próximo à", "89036789",
+				"Bom Retiro", "Brasil", "Blumenau", "SC");
+		List<Empresa> listaEmpresas = EmpresaController.buscarEmpresaPorNome("Magalu");
+		assertEquals(2, listaEmpresas.size());
 	}
 
 	@Before
