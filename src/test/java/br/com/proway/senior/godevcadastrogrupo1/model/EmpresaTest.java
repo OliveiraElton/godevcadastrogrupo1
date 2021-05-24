f526ba35e987b275e74f1d35f3f457a7bfb2700d
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-24 15:49:40 -0300

    Finalizando testes classe Empresa

--- src/test/java/br/com/proway/senior/godevcadastrogrupo1/model/EmpresaTest.java
+++ src/test/java/br/com/proway/senior/godevcadastrogrupo1/model/EmpresaTest.java
@@ -16,53 +16,52 @@ import org.junit.Test;
  *
  */
 public class EmpresaTest {
-	
-	Endereco endereco = new Endereco("Rua XV", 123, "Casa", "89035183", "Centro", 
-			"Brasil", "Blumenau", "SC");
+
+	Endereco endereco = new Endereco("Rua XV", 123, "Casa", "89035183", "Centro", "Brasil", "Blumenau", "SC");
 	Contatos contatos = new Contatos("47984563214", "47985632145", "proway@proway.com", "47985632144");
 
 	@Test
 	public void testConstrutor() {
-		Empresa empresa = new Empresa("Proway", LocalDate.of(2021, 12, 05), "99.272.968/0001-34", 
-				endereco, contatos);
+		Empresa empresa = new Empresa("Proway", LocalDate.of(2021, 12, 05), "99.272.968/0001-34", endereco, contatos);
 		assertNotNull(empresa);
 	}
-	
-	@Test 
+
+	@Test
 	public void testSetEGetId() {
 		Empresa empresa = new Empresa();
-		empresa.setId(150);;
+		empresa.setId(150);
+		;
 		assertEquals((Integer) 150, empresa.getId());
 	}
-	
-	@Test 
+
+	@Test
 	public void testSetEGetNomeEmpresa() {
 		Empresa empresa = new Empresa();
 		empresa.setNomeEmpresa("Proway");
 		assertEquals("Proway", empresa.getNomeEmpresa());
 	}
 
-	@Test 
+	@Test
 	public void testSetEGetDataInicio() {
 		Empresa empresa = new Empresa();
 		empresa.setDataInicioContrato(LocalDate.of(2021, 10, 15));
 		assertEquals(LocalDate.of(2021, 10, 15), empresa.getDataInicioContrato());
 	}
-	
-	@Test 
+
+	@Test
 	public void testSetEGetCNPJCorreto() throws Exception {
 		Empresa empresa = new Empresa();
 		empresa.setCnpj("05975585000189");
 		assertEquals("05975585000189", empresa.getCnpj());
 	}
-	
-	@Test (expected = Exception.class)
+
+	@Test(expected = Exception.class)
 	public void testSetEGetCNPJIncorreto() throws Exception {
 		Empresa empresa = new Empresa();
 		empresa.setCnpj("819930900001");
 	}
-	
-	@Test 
+
+	@Test
 	public void testSetEGetEndereco() {
 		Empresa empresa = new Empresa();
 		empresa.setEndereco(endereco);
@@ -70,8 +69,8 @@ public class EmpresaTest {
 		assertEquals(endereco.getLogradouro(), empresa.getEndereco().getLogradouro());
 		assertEquals(endereco.getComplemento(), empresa.getEndereco().getComplemento());
 	}
-	
-	@Test 
+
+	@Test
 	public void testSetEGetContatos() {
 		Empresa empresa = new Empresa();
 		empresa.setContato(contatos);
@@ -79,5 +78,5 @@ public class EmpresaTest {
 		assertEquals(contatos.getEmail(), empresa.getContato().getEmail());
 		assertEquals(contatos.getTelefoneFamiliar(), empresa.getContato().getTelefoneFamiliar());
 	}
-	
+
 }
