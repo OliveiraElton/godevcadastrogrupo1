c2ca767a8aa76884950e3f5a78f61b3bdb71d2bf
Lucas Walim <lucaswalim@hotmail.com>
2021-05-24 17:12:10 -0300

    exclusao metodo limparTabela PrestadorServicoControlelr

--- src/main/java/br/com/proway/senior/godevcadastrogrupo1/model/DAO/PrestadorServicoDAO.java
+++ src/main/java/br/com/proway/senior/godevcadastrogrupo1/model/DAO/PrestadorServicoDAO.java
@@ -76,23 +76,6 @@ public class PrestadorServicoDAO extends Dao<PrestadorServico> implements Interf
 			List<PrestadorServico> results = query.getResultList();
 			return new ArrayList<PrestadorServico>(results);
 		}
-	public boolean limparTabela() {
-		if (!session.getTransaction().isActive()) {
-			session.beginTransaction();
-		}
-		try {
-		
-			CriteriaBuilder builder = session.getCriteriaBuilder();
-			CriteriaDelete<PrestadorServico> criteriaDelete = builder.createCriteriaDelete(PrestadorServico.class);
-			criteriaDelete.from(PrestadorServico.class);
-			Query query = session.createQuery(criteriaDelete);
-			query.executeUpdate();
-			return true;
-		} catch (Exception e) {
-			e.getMessage();
-			return false;
-		}
-	}
 	
 	public boolean deleteAll() {
 		if (!this.session.getTransaction().isActive()) {
