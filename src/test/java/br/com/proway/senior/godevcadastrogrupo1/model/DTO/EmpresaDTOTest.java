a717151fe0121cc1fa7aa0edd1db39d32bf399d4
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-25 17:01:13 -0300

    Testes dos métodos delete, create e update

--- src/test/java/br/com/proway/senior/godevcadastrogrupo1/model/DTO/EmpresaDTOTest.java
+++ src/test/java/br/com/proway/senior/godevcadastrogrupo1/model/DTO/EmpresaDTOTest.java
@@ -13,15 +13,15 @@ import br.com.proway.senior.godevcadastrogrupo1.model.Endereco;
 /**
  * Classe EmpresaDTOTest.
  * 
- * Testa os métodos da classe {@link EmpresaDTO}.
+ * Testa os mï¿½todos da classe {@link EmpresaDTO}.
  * 
  * @author Sarah Neuburger Brito <b>sarah.brito@senior.com.br</b>
  */
 public class EmpresaDTOTest {
 
 	@Test
-	public void testEmpresaDTO() {
-		Empresa original = new Empresa("Selecionar", LocalDate.of(2021, 04, 06), "89123654000123", new Endereco(), new Contatos());
+	public void testEmpresaDTO() throws Exception {
+		Empresa original = new Empresa("Selecionar", LocalDate.of(2021, 04, 06), "05.975.585/0001-89", new Endereco(), new Contatos());
 		EmpresaDTO dto = new EmpresaDTO(original);
 		assertEquals(0, dto.getId());
 		assertEquals(original.getNomeEmpresa(), dto.getNomeEmpresa());
