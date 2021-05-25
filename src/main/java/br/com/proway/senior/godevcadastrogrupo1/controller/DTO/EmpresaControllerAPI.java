fa0c38d3ae9e51e8bf1dc3b8d8ac15c240c6b17e
Vitor <vitorperes1104@gmail.com>
2021-05-25 11:26:15 -0300

    Anotações spring classe EmpresaControllerAPI[

--- src/main/java/br/com/proway/senior/godevcadastrogrupo1/controller/DTO/EmpresaControllerAPI.java
+++ src/main/java/br/com/proway/senior/godevcadastrogrupo1/controller/DTO/EmpresaControllerAPI.java
@@ -3,34 +3,65 @@ package br.com.proway.senior.godevcadastrogrupo1.controller.DTO;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.proway.senior.godevcadastrogrupo1.controller.EmpresaController;
 import br.com.proway.senior.godevcadastrogrupo1.model.Empresa;
+import br.com.proway.senior.godevcadastrogrupo1.model.DAO.EmpresaDAO;
 import br.com.proway.senior.godevcadastrogrupo1.model.DTO.EmpresaDTO;
+import br.com.proway.senior.godevcadastrogrupo1.persistence.DBConnection;
 
 /**
  * Classe EmpresaControllerAPI
  * 
  * Classe disponibilizada para consulta dos dados da empresa via API Rest.
- * Implementa os métodos de visualização do {@link EmpresaController} e atriutos
+ * Implementa os mï¿½todos de visualizaï¿½ï¿½o do {@link EmpresaController} e atriutos
  * do model {@link EmpresaDTO}.
  * 
  * @author Sarah Neuburger Brito <b>sarah.brito@senior.com.br</b>
  */
+@RestController
 public class EmpresaControllerAPI {
+	
+	static Session session = DBConnection.getSession();
+	static EmpresaDAO daoEmpresa = EmpresaDAO.getInstance(session);
 
 	static EmpresaController controllerOriginal = new EmpresaController();
 
+	@RequestMapping(value = "/empresa", method = RequestMethod.POST)
+	public @ResponseBody Empresa criarEmpresa(@RequestBody Empresa empresa) {
+		return daoEmpresa.create(empresa);
+	}
+	
+	@RequestMapping(value = "/empresa/{id}", method = RequestMethod.DELETE)
+	public @ResponseBody boolean deleteEmpresa(@PathVariable ("id") Integer id) {
+		Empresa empresa = daoEmpresa.readById(id);
+		return daoEmpresa.delete(empresa);
+	}
+	
+	@RequestMapping(value = "/empresa/{id}", method = RequestMethod.PUT)
+	public @ResponseBody Empresa atualizarEmpresa(@PathVariable ("id") Integer id, @RequestBody Empresa empresa) {
+		empresa.setId(id);
+		return daoEmpresa.update(empresa);
+	}
 	/**
 	 * Buscar empresa por Id.
 	 * 
-	 * Método busca as informações cadastradas da empresa, conforme idEmpresa
-	 * informado. Retorna um objeto EmpresaDTO que será visualizado pelo cliente da
+	 * Mï¿½todo busca as informaï¿½ï¿½es cadastradas da empresa, conforme idEmpresa
+	 * informado. Retorna um objeto EmpresaDTO que serï¿½ visualizado pelo cliente da
 	 * API.
 	 * 
-	 * @param idEmpresa identificação da empresa que será retornada.
-	 * @return EmpresaDTO objeto com as informações do banco.
+	 * @param idEmpresa identificaï¿½ï¿½o da empresa que serï¿½ retornada.
+	 * @return EmpresaDTO objeto com as informaï¿½ï¿½es do banco.
 	 */
-	public EmpresaDTO buscarEmpresaPorId(Integer idEmpresa) {
+	@RequestMapping(value = "/empresa/{id}", method = RequestMethod.GET)
+	public @ResponseBody EmpresaDTO buscarEmpresaPorId(@PathVariable ("id") Integer idEmpresa) {
 		EmpresaDTO empresaDTO = new EmpresaDTO(controllerOriginal.buscarEmpresaPorId(idEmpresa));
 		return empresaDTO;
 	}
@@ -38,12 +69,13 @@ public class EmpresaControllerAPI {
 	/**
 	 * Buscar todas as empresas.
 	 * 
-	 * Método busca as informações de todas as empresas cadastradas no banco de
+	 * Mï¿½todo busca as informaï¿½ï¿½es de todas as empresas cadastradas no banco de
 	 * dados. Retorna uma lista de todos os registros de empresas.
 	 * 
 	 * @return listaEmpresaDTO lista de registros localizados.
 	 */
-	public static List<EmpresaDTO> buscarTodasEmpresas() {
+	@RequestMapping(value = "/empresa", method = RequestMethod.GET)
+	public @ResponseBody List<EmpresaDTO> buscarTodasEmpresas() {
 		List<EmpresaDTO> listaEmpresaDTO = new ArrayList<EmpresaDTO>();
 		List<Empresa> listaImprime = controllerOriginal.buscarTodasEmpresas();
 		for (Empresa empresa : listaImprime) {
@@ -55,16 +87,18 @@ public class EmpresaControllerAPI {
 	/**
 	 * Busca empresa por nome.
 	 * 
-	 * Método busca as empresas no banco de dados através dos seus respectivos
-	 * nomes, é possível passar um parâmetro parcial para retorna todos os registros
+	 * Mï¿½todo busca as empresas no banco de dados atravï¿½s dos seus respectivos
+	 * nomes, ï¿½ possï¿½vel passar um parï¿½metro parcial para retorna todos os registros
 	 * que contenham determinado texto em seu nomeEmpresa.
 	 * 
-	 * @param nomeEmpresa nome dos registros que estão sendo procurados.
+	 * @param nomeEmpresa nome dos registros que estï¿½o sendo procurados.
 	 * @return ArrayList Empresa lista de registros localizados.
 	 */
-	public static List<EmpresaDTO> buscarEmpresaPorNome(String nomeEmpresa) {
+	@RequestMapping(value = "/empresa/{nome}", method = RequestMethod.GET)
+	public @ResponseBody List<EmpresaDTO> buscarEmpresaPorNome(@PathVariable ("nome") 
+			String nome) {
 		List<EmpresaDTO> listaEmpresaDTO = new ArrayList<EmpresaDTO>();
-		for (Empresa empresa : controllerOriginal.buscarEmpresaPorNome(nomeEmpresa)) {
+		for (Empresa empresa : controllerOriginal.buscarEmpresaPorNome(nome)) {
 			listaEmpresaDTO.add(new EmpresaDTO(empresa));
 		}
 		return listaEmpresaDTO;
