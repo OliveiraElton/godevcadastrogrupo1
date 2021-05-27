571005ba65edef1e54951aec49b4282506b489dd
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-27 13:04:35 -0300

    Ajustes nas documentações dos Enums

--- src/main/java/br/com/proway/senior/godevcadastrogrupo1/utils/EnumDadosPessoais.java
+++ src/main/java/br/com/proway/senior/godevcadastrogrupo1/utils/EnumDadosPessoais.java
@@ -1,31 +1,24 @@
 package br.com.proway.senior.godevcadastrogrupo1.utils;
 
 /**
-	 * Classe de apoio para ser utilizada em Pessoa para 
-	 * setar o sexo.
-	 * 
-	 * @author Bruna <sh4323202@gmail.com>
-	 * @author Enzo <enzomm.bodyandmind@gmail.com> 
-	 * @author Sabrina <sabrinaschmidt335@gmail.com>
-	 * @author Vanderlei <vanderleik@yahoo.com.br>
-	 * @author Vitor <vitornathang@gmail.com>
-	 */
-/**
- * @author Senior
- *
+ * Classe de apoio para ser utilizada em {@link Pessoa} para setar a
+ * identidade de genero e em {@link Dependente} para setar o tipos de 
+ * depenente.
+ * 
+ * @author Bruna <sh4323202@gmail.com>
+ * @author Enzo <enzomm.bodyandmind@gmail.com>
+ * @author Sabrina <sabrinaschmidt335@gmail.com>
+ * @author Vanderlei <vanderleik@yahoo.com.br>
+ * @author Vitor <vitornathang@gmail.com>
  */
+
 public class EnumDadosPessoais {
+	
 	
 	public enum IdentidadeGenero {
 		TRANS, CIS
 	}
 
-	/**
-	 * Classe de apoio para ser utilizada em Dependentes para
-	 * ser o tipo do mesmo.
-	 * 
-	 * @author Vanderlei, Vitor Gon�alves, Sabrina, Enzo Moura e Bruna.
-	 */
 	public enum TiposDependentes {
 		FILHO, CONJUGE, MAE, PAI
 	}
