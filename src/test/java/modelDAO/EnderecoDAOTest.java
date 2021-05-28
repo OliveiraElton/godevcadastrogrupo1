93f1cc3d71984f13a30be0f4ffd565da32fe4287
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-05-28 16:57:38 -0300

    Ajustes ControllersAPI e DTOs Testes

--- src/test/java/modelDAO/EnderecoDAOTest.java
+++ src/test/java/modelDAO/EnderecoDAOTest.java
@@ -3,7 +3,6 @@ package modelDAO;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import java.time.LocalDate;
 
@@ -38,14 +37,13 @@ public class EnderecoDAOTest {
 	ExameMedico exameMedico = new ExameMedico(em, LocalDate.now(), true);
 	Endereco endereco = new Endereco("Rua XV de Novembro", 154, "Casa", "89065544", "Centro", "Brasil", "Blumenau",
 			"SC");
-	Dependente dependente = new Dependente("Joao", "Fonseca", "Jenifer", data, "Venezuelano", "Cidade del Leste",
-			true, null, null, endereco, "09619039610", null, null, true);
+	Dependente dependente = new Dependente("Joao", "Fonseca", data, "Venezuelano", true, null, null, "09619039610",
+			null, true);
 
-	
 	@Test
 	public void testReadById() {
-		Endereco endereco = new Endereco("Rua joao pessoa", 985, "lado esquerdo da rua", "8965123", 
-				"Centro", "Brasil", "Blumenau", "SC");
+		Endereco endereco = new Endereco("Rua joao pessoa", 985, "lado esquerdo da rua", "8965123", "Centro", "Brasil",
+				"Blumenau", "SC");
 		dao.cadastrar(endereco);
 		assertEquals(endereco, dao.buscarPorId(Endereco.class, endereco.getId()));
 	}
@@ -62,10 +60,9 @@ public class EnderecoDAOTest {
 
 	}
 
-
 	@Test
-	public void testCreate() throws Exception{
-		Endereco endereco = new Endereco(); 
+	public void testCreate() throws Exception {
+		Endereco endereco = new Endereco();
 		endereco.setComplemento("Perto do posto de saude");
 		endereco.setLogradouro("Rua 7");
 		endereco.setNumero(2);
@@ -75,7 +72,7 @@ public class EnderecoDAOTest {
 		endereco.setCidade("Blumenau");
 		endereco.setUf("SC");
 		dao.cadastrar(endereco);
-		
+
 		assertEquals("Perto do posto de saude", endereco.getComplemento());
 		assertEquals("Rua 7", endereco.getLogradouro());
 		assertEquals(Integer.valueOf(2), endereco.getNumero());
@@ -86,14 +83,14 @@ public class EnderecoDAOTest {
 		assertEquals("SC", endereco.getUf());
 
 	}
-	
+
 	@Test
 	public void testDelete() {
 		Endereco endereco = new Endereco("Rua 7 de Setembro", 45, "", "8974335", "Centro", "Brasil", "Blumenau", "SC");
 		dao.cadastrar(endereco);
-		
+
 		Integer valorAntes = dao.buscarTodos(Endereco.class).size();
-		
+
 		dao.deletar(endereco);
 		assertEquals(valorAntes - 1, dao.buscarTodos(Endereco.class).size());
 	}
@@ -104,26 +101,26 @@ public class EnderecoDAOTest {
 		dao.deletarTodos("endereco");
 		assertFalse(dao.buscarTodos(Endereco.class).size() > 0);
 	}
-	
+
 	@Test
 	public void testBuscarEnderecoPorIdColaborador() {
 		Endereco endereco = new Endereco("Rua 7 de Setembro", 45, "", "8974335", "Centro", "Brasil", "Blumenau", "SC");
-		
+
 		Colaborador colaborador = new Colaborador("Carla", "Nunes", "Nada consta", data, "Americana", "Los Angeles",
 				false, "Feminino", ig, endereco, "21164028324", "45124563", contatos, null, null, false, false, data,
 				false, null, "maria.nunes@gmail.com", "554555", conta, exameMedico, dependente);
-		
+
 		Endereco enderecoCadastrado = dao.cadastrar(endereco);
 		Endereco colaboradorCadastrado = dao.buscarPorId(Endereco.class, enderecoCadastrado.getId());
 		assertEquals(enderecoCadastrado.getId(), colaboradorCadastrado.getId());
 		assertNotNull(enderecoCadastrado);
 		assertNotNull(colaboradorCadastrado);
-		//assertNull(enderecoCadastrado);
+		// assertNull(enderecoCadastrado);
 	}
-	
+
 	@Before
 	public void limparTabelas() {
 		dao.deletarTodos("endereco");
 	}
-	
+
 }
